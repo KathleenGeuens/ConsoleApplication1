19bbf094da7b88094fa24481648b0a096996542a
geuensk1115 <geuensk1115@COL-B030109>
2017-01-11 11:46:49 +0100

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declareren van variabelen en constanten
+            double aantalLeeuwen = 50;
+            const double groeiPercentage = 0.15;
+            int jaar = 0;
+
+            while (aantalLeeuwen<100)
+            {
+                Console.WriteLine("Na {0} jaar zijn er {1} leeuwen.", jaar,Math.Round(aantalLeeuwen));
+                aantalLeeuwen = aantalLeeuwen + aantalLeeuwen * groeiPercentage;
+                //aantalLeeuwen = aantalLeeuwen * (1 + groeiPercentage);
+                jaar++;
+                //jaar = jaar + 1;
+            }
+            Console.WriteLine("Dus na {0} jaar zijn er meer dan 100 leeuwen, namelijk: {1} leeuwen.", jaar, Math.Round(aantalLeeuwen));
+            Console.ReadKey();
+        }
+    }
+}
